bc09a8c61b397ae2a5a0d6aa64b8ad5e23eb7739
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-03 19:40:12 +0530

    Dhruv Sharma add: rocket count down

---
+++ RocketCountdown.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; 
+
+class RocketCountdown {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        // user to enter the countdown start value
+        System.out.print("Enter the countdown start value: ");
+        int counter = sc.nextInt(); 
+
+        // Countdown from the user input value to 1
+        while (counter>=1) {
+            System.out.println(counter);
+            counter--; // Decrement the counter
+        }
+
+    }
+}
